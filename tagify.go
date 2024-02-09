15267e41f8f7eeef5701803fd6a7c1535a660f89
Eric Haughee <ehaughee@gmail.com>
2024-02-08 19:54:35 -0800

    Add login, logout, and sessions

--- tagify.go
+++ tagify.go
@@ -3,7 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
@@ -11,9 +14,14 @@ import (
 
 const (
 	// TODO: Parameterize this
-	authRedirectURL    = "http://localhost:8080/auth_redir"
+	authRedirectURL = "http://localhost:8080/auth_redir"
+
 	SPOTIFY_ID_ENV     = "SPOTIFY_ID"
 	SPOTIFY_SECRET_ENV = "SPOTIFY_SECRET"
+
+	COOKIE_AUTH_KEY = "COOKIE_AUTH_KEY"
+
+	sessionTokenKey = "session-token"
 )
 
 var (
@@ -32,11 +40,17 @@ func main() {
 	auth = spotifyauth.New(
 		spotifyauth.WithRedirectURL(authRedirectURL),
 		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate),
-		// spotifyauth.WithClientID(os.Getenv(SPOTIFY_ID_ENV)),
-		// spotifyauth.WithClientSecret(os.Getenv(SPOTIFY_SECRET_ENV)),
 	)
 
 	r := gin.Default()
+
+	// Setup session management
+	cookieAuthKey := os.Getenv(COOKIE_AUTH_KEY)
+	if cookieAuthKey == "" {
+		log.Fatalf("Required environment variable %q was empty", COOKIE_AUTH_KEY)
+	}
+	store := cookie.NewStore([]byte(cookieAuthKey))
+	r.Use(sessions.Sessions("user_sessions", store))
 
 	setupRoutes(r)
 
@@ -44,9 +58,26 @@ func main() {
 }
 
 func setupRoutes(r *gin.Engine) {
+	r.GET("/", rootHandler)
+	r.GET("/home", homeHandler)
 	r.GET("/ping", pingHandler)
 	r.GET("/auth_redir", authRedirectHandler)
 	r.GET("/login", loginHandler)
+	r.GET("/logout", logoutHandler)
+}
+
+func rootHandler(c *gin.Context) {
+	c.Data(http.StatusOK, "text/plain", []byte("ok"))
+}
+
+func homeHandler(c *gin.Context) {
+	if !loggedIn(c) {
+		c.Redirect(http.StatusTemporaryRedirect, "/login")
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": getSessionToken(c),
+	})
 }
 
 func pingHandler(c *gin.Context) {
@@ -56,9 +87,22 @@ func pingHandler(c *gin.Context) {
 }
 
 func loginHandler(c *gin.Context) {
-	// TODO: Introduce sessions for handling state strings
-	log.Printf("Auth URL: %s", auth.AuthURL("test"))
-	c.Redirect(http.StatusMovedPermanently, auth.AuthURL("test"))
+	// Check if we already have a token, meaning we're already logged in
+	// TODO: Handle token expiry as well as near-expiry
+	if !loggedIn(c) {
+		log.Printf("Auth URL: %s", auth.AuthURL("test"))
+		c.Redirect(http.StatusMovedPermanently, auth.AuthURL("test"))
+	}
+
+	c.Redirect(http.StatusMovedPermanently, "/home")
+}
+
+func logoutHandler(c *gin.Context) {
+	if loggedIn(c) {
+		logOut(c)
+	}
+
+	c.Redirect(http.StatusTemporaryRedirect, "/")
 }
 
 func authRedirectHandler(c *gin.Context) {
@@ -70,7 +114,31 @@ func authRedirectHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"token": token,
-	})
+	setSessionToken(token.AccessToken, c)
+	c.Redirect(http.StatusTemporaryRedirect, "/home")
+}
+
+func loggedIn(c *gin.Context) bool {
+	return getSessionToken(c) != ""
+}
+
+func getSessionToken(c *gin.Context) string {
+	token := sessions.Default(c).Get(sessionTokenKey)
+	if token == nil {
+		return ""
+	}
+
+	return token.(string)
+}
+
+func setSessionToken(token string, c *gin.Context) {
+	session := sessions.Default(c)
+	session.Set(sessionTokenKey, token)
+	session.Save()
+}
+
+func logOut(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Delete(sessionTokenKey)
+	session.Save()
 }
