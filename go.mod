a046b07123c25bbdaba1fb26b3786d9ae19301da
Eric Haughee <ehaughee@gmail.com>
2024-02-08 19:11:04 -0800

    Initial Spotify client auth and dotenv

--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.17.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -22,11 +24,14 @@ require (
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/zmb3/spotify/v2 v2.4.1 // indirect
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tagify.go
+++ tagify.go
@@ -1,14 +1,76 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+const (
+	// TODO: Parameterize this
+	authRedirectURL    = "http://localhost:8080/auth_redir"
+	SPOTIFY_ID_ENV     = "SPOTIFY_ID"
+	SPOTIFY_SECRET_ENV = "SPOTIFY_SECRET"
+)
+
+var (
+	auth *spotifyauth.Authenticator
+)
 
 func main() {
+	// Load dev environment variables
+	// TODO: Conditionally do this depending on the Gin mode (dev vs release)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Create new Spotify Authenticator.  This must be done after we've loaded the environment variables.
+	auth = spotifyauth.New(
+		spotifyauth.WithRedirectURL(authRedirectURL),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate),
+		// spotifyauth.WithClientID(os.Getenv(SPOTIFY_ID_ENV)),
+		// spotifyauth.WithClientSecret(os.Getenv(SPOTIFY_SECRET_ENV)),
+	)
+
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	setupRoutes(r)
 
 	r.Run()
+}
+
+func setupRoutes(r *gin.Engine) {
+	r.GET("/ping", pingHandler)
+	r.GET("/auth_redir", authRedirectHandler)
+	r.GET("/login", loginHandler)
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func loginHandler(c *gin.Context) {
+	// TODO: Introduce sessions for handling state strings
+	log.Printf("Auth URL: %s", auth.AuthURL("test"))
+	c.Redirect(http.StatusMovedPermanently, auth.AuthURL("test"))
+}
+
+func authRedirectHandler(c *gin.Context) {
+	token, err := auth.Token(c.Request.Context(), "test", c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
 }
