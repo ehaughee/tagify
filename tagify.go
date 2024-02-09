7c9d800c13ab559300a01082dd44d91aa33d82ba
Eric Haughee <ehaughee@gmail.com>
2024-02-08 17:44:01 -0800

    Initial Gin commit

--- tagify.go
+++ tagify.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	println("sup")
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
