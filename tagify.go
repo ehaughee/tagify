6cd47ae4393b9b687b7ddcfe5abd7c3cd232cfca
Eric Haughee <ehaughee@gmail.com>
2025-03-01 18:07:07 -0800

    Add picocss theme

--- tagify.go
+++ tagify.go
@@ -56,6 +56,7 @@ func main() {
 
 	// Static files
 	r.StaticFile("/favicon.ico", "./static/favicon.ico")
+	r.Static("/css", "./static/css")
 
 	// Load templates
 	r.HTMLRender = loadTemplates("./templates")
